209674cd665869322f6a66cbd57789b9ca1c6ea5
account1 <mochammad.lutfi@bluebirdgroup.com>
2022-04-09 20:39:30 +0700

    installation and configuration expressjs mongodb

---
+++ db/index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const { urlDb } = require('../config')
+
+mongoose.connect(urlDb, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+
+const db = mongoose.connection
+
+module.exports = db
