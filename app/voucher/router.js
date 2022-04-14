76ae268063edcbd5ebdbe837bd799326ce251480
account1 <mochammad.lutfi@bluebirdgroup.com>
2022-04-14 22:20:55 +0700

    handle create voucher

---
+++ app/voucher/router.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+const { index, viewCreate, actionCreate } = require('./controller');
+const multer = require('multer');
+const os = require('os');
+
+router.get('/', index);
+router.get('/create', viewCreate);
+router.post('/create', multer({dest: os.tmpdir()}).single('image'), actionCreate);
+// router.get('/edit/:id', viewEdit);
+// router.put('/edit/:id', actionEdit);
+// router.delete('/delete/:id', actionDelete);
+
+module.exports = router;
