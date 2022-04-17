6f40d13dbe9fba53b87ea916231df64dd40721e1
account1 <mochammad.lutfi@bluebirdgroup.com>
2022-04-17 09:29:12 +0700

    handle update status voucher

--- app/voucher/router.js
+++ app/voucher/router.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const { index, viewCreate, actionCreate, viewEdit, actionEdit, actionDelete } = require('./controller');
+const { index, viewCreate, actionCreate, viewEdit, actionEdit, actionDelete, actionStatus } = require('./controller');
 const multer = require('multer');
 const os = require('os');
 
@@ -10,5 +10,6 @@ router.post('/create', multer({dest: os.tmpdir()}).single('image'), actionCreate
 router.get('/edit/:id', viewEdit);
 router.put('/edit/:id', multer({dest: os.tmpdir()}).single('image'), actionEdit);
 router.delete('/delete/:id', actionDelete);
+router.put('/status/:id', actionStatus);
 
 module.exports = router;
