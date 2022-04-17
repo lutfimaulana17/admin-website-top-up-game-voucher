31f53beb7bdff1cdb4011af25979ed8f72f85e6b
account1 <mochammad.lutfi@bluebirdgroup.com>
2022-04-17 11:39:36 +0700

    handle update data payment

--- app/payment/controller.js
+++ app/payment/controller.js
@@ -48,38 +48,40 @@ module.exports={
             res.redirect('/payment')
         }
     },
-    // viewEdit: async (req, res) => {
-    //     try {
-    //         const { id } = req.params
-    //         const nominal = await Nominal.findOne({ _id: id })
-            
-    //         res.render('admin/nominal/edit', {
-    //             nominal
-    //         })
-    //     } catch (err) {
-    //         req.flash('alertMessage', `${err.message}`)
-    //         req.flash('alertStatus', 'danger')
-    //         res.redirect('/nominal')
-    //     }
-    // },
-    // actionEdit: async (req, res) => {
-    //     try {
-    //         const { id } = req.params
-    //         const { coinName, coinQuantity, price } = req.body
-    //         await Nominal.findOneAndUpdate({
-    //             _id: id
-    //         },{ coinName, coinQuantity, price })
+    viewEdit: async (req, res) => {
+        try {
+            const { id } = req.params
+            const payment = await Payment.findOne({ _id: id }).populate('banks')
+            const banks = await Bank.find()
 
-    //         req.flash('alertMessage', 'Berhasil ubah nominal')
-    //         req.flash('alertStatus', 'success')
+            res.render('admin/payment/edit', {
+                payment,
+                banks
+            })
+        } catch (err) {
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/payment')
+        }
+    },
+    actionEdit: async (req, res) => {
+        try {
+            const { id } = req.params
+            const { banks, type } = req.body
+            await Payment.findOneAndUpdate({
+                _id: id
+            },{ banks, type })
+
+            req.flash('alertMessage', 'Berhasil ubah payment')
+            req.flash('alertStatus', 'success')
             
-    //         res.redirect('/nominal')
-    //     } catch (err) {
-    //         req.flash('alertMessage', `${err.message}`)
-    //         req.flash('alertStatus', 'danger')
-    //         res.redirect('/nominal')
-    //     }
-    // },
+            res.redirect('/payment')
+        } catch (err) {
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/payment')
+        }
+    },
     // actionDelete: async (req, res) => {
     //     try {  
     //         const { id } = req.params
--- app/payment/router.js
+++ app/payment/router.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const { index, viewCreate, actionCreate } = require('./controller');
+const { index, viewCreate, actionCreate, viewEdit, actionEdit } = require('./controller');
 
 router.get('/', index);
 router.get('/create', viewCreate);
 router.post('/create', actionCreate);
-// router.get('/edit/:id', viewEdit);
-// router.put('/edit/:id', actionEdit);
+router.get('/edit/:id', viewEdit);
+router.put('/edit/:id', actionEdit);
 // router.delete('/delete/:id', actionDelete);
 
 module.exports = router;
