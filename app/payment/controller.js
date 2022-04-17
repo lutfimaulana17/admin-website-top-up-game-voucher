c3494dc2cd97e13ae554e4e5cec9e423c300f54a
account1 <mochammad.lutfi@bluebirdgroup.com>
2022-04-17 13:18:53 +0700

    handle delete payment

--- app/payment/controller.js
+++ app/payment/controller.js
@@ -82,21 +82,21 @@ module.exports={
             res.redirect('/payment')
         }
     },
-    // actionDelete: async (req, res) => {
-    //     try {  
-    //         const { id } = req.params
-    //         await Nominal.findOneAndRemove({
-    //             _id: id
-    //         })
+    actionDelete: async (req, res) => {
+        try {  
+            const { id } = req.params
+            await Payment.findOneAndRemove({
+                _id: id
+            })
 
-    //         req.flash('alertMessage', 'Berhasil hapus nominal')
-    //         req.flash('alertStatus', 'success')
+            req.flash('alertMessage', 'Berhasil hapus payment')
+            req.flash('alertStatus', 'success')
 
-    //         res.redirect('/nominal')
-    //     } catch (err) {
-    //         req.flash('alertMessage', `${err.message}`)
-    //         req.flash('alertStatus', 'danger')
-    //         res.redirect('/nominal')
-    //     }
-    // }
+            res.redirect('/payment')
+        } catch (err) {
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/payment')
+        }
+    }
 }
