edcb6c1c4e4ca223f557e42ba1a90fac0a317dc1
account1 <mochammad.lutfi@bluebirdgroup.com>
2022-04-17 10:24:51 +0700

    handle delete data bank

--- app/bank/controller.js
+++ app/bank/controller.js
@@ -75,21 +75,21 @@ module.exports={
             res.redirect('/bank')
         }
     },
-    // actionDelete: async (req, res) => {
-    //     try {  
-    //         const { id } = req.params
-    //         await Nominal.findOneAndRemove({
-    //             _id: id
-    //         })
+    actionDelete: async (req, res) => {
+        try {  
+            const { id } = req.params
+            await Bank.findOneAndRemove({
+                _id: id
+            })
 
-    //         req.flash('alertMessage', 'Berhasil hapus nominal')
-    //         req.flash('alertStatus', 'success')
+            req.flash('alertMessage', 'Berhasil hapus bank')
+            req.flash('alertStatus', 'success')
 
-    //         res.redirect('/nominal')
-    //     } catch (err) {
-    //         req.flash('alertMessage', `${err.message}`)
-    //         req.flash('alertStatus', 'danger')
-    //         res.redirect('/nominal')
-    //     }
-    // }
+            res.redirect('/bank')
+        } catch (err) {
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/bank')
+        }
+    }
 }
