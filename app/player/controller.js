73b7baed2bb9819febdb3d1e98a3dc595e2e3d96
account1 <mochammad.lutfi@bluebirdgroup.com>
2022-04-25 21:14:47 +0700

    create api category

--- app/player/controller.js
+++ app/player/controller.js
@@ -1,5 +1,6 @@
 const Player = require('./model')
 const Voucher = require('../voucher/model')
+const Category = require('../category/model')
 
 module.exports={
     landingPage: async (req, res) => {
@@ -32,6 +33,18 @@ module.exports={
 
             res.status(200).json({
                 data: voucher
+            })
+        } catch (err) {
+            res.status(500).json({
+                message: err.message || 'Internal server error'
+            })
+        }
+    },
+    category: async (req, res) => {
+        try {
+            const category = await Category.find()
+            res.status(200).json({
+                data: category
             })
         } catch (err) {
             res.status(500).json({
