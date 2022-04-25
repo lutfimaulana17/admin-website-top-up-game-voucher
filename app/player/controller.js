8f18feb9919b0b769666dedefe4d6a34f229c485
account1 <mochammad.lutfi@bluebirdgroup.com>
2022-04-25 15:55:19 +0700

    create api detail page

--- app/player/controller.js
+++ app/player/controller.js
@@ -12,7 +12,30 @@ module.exports={
             })
         } catch (err) {
             res.status(500).json({
-                message: err.message || 'Kesalahan pada server'
+                message: err.message || 'Internal server error'
+            })
+        }
+    },
+    detailPage: async (req, res) => {
+        try {
+            const { id } = req.params
+            const voucher = await Voucher.findOne({ _id: id })                
+                .populate('category')
+                .populate('nominals')
+                .populate('user', '_id name phoneNumber')
+
+            if (!voucher) {
+                return res.status(404).json({
+                    message: 'Voucher game tidak ditemukan!'
+                })
+            }
+
+            res.status(200).json({
+                data: voucher
+            })
+        } catch (err) {
+            res.status(500).json({
+                message: err.message || 'Internal server error'
             })
         }
     }
