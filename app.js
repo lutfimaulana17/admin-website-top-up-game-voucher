3e42ba16fbad453fe7cc0c975aa827726aba21da
account1 <mochammad.lutfi@bluebirdgroup.com>
2022-04-25 15:06:31 +0700

    create api landing page

--- app.js
+++ app.js
@@ -15,8 +15,10 @@ const bankRouter = require('./app/bank/router');
 const paymentRouter = require('./app/payment/router');
 const usersRouter = require('./app/users/router');
 const transactionRouter = require('./app/transaction/router');
+const playerRouter = require('./app/player/router');
 
 const app = express();
+const URL = '/api/v1'
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -45,6 +47,9 @@ app.use('/voucher', voucherRouter);
 app.use('/bank', bankRouter);
 app.use('/payment', paymentRouter);
 app.use('/transaction', transactionRouter);
+
+//API
+app.use(`${URL}/players`, playerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ app/player/controller.js
@@ -0,0 +1,19 @@
+const Player = require('./model')
+const Voucher = require('../voucher/model')
+
+module.exports={
+    landingPage: async (req, res) => {
+        try {
+            const voucher = await Voucher.find()
+                .select('_id name status category thumbnail')
+                .populate('category')
+            res.status(200).json({
+                data: voucher
+            })
+        } catch (err) {
+            res.status(500).json({
+                message: err.message || 'Kesalahan pada server'
+            })
+        }
+    }
+}
