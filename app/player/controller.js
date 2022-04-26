b485e6b972d913c6cf2f8c34e6c6996b183ceea0
account1 <mochammad.lutfi@bluebirdgroup.com>
2022-04-26 11:18:20 +0700

    create api transaction detail

--- app/player/controller.js
+++ app/player/controller.js
@@ -160,5 +160,21 @@ module.exports={
                 message: err.message || 'Internal server error'
             })
         }
+    },
+    historyDetail: async (req, res) => {
+        try {
+            const { id } = req.params
+            const history = await Transaction.findOne({ _id: id })
+
+            if (!history) return res.status(404).json({ message: 'History tidak ditemukan.' })
+
+            res.status(200).json({
+                data: history
+            })
+        } catch (err) {
+            res.status(500).json({
+                message: err.message || 'Internal server error'
+            })
+        }
     }
 }
--- app/player/router.js
+++ app/player/router.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const { landingPage, detailPage, category, checkout, history } = require('./controller')
+const { landingPage, detailPage, category, checkout, history, historyDetail } = require('./controller')
 const { isLoginPlayer } = require('../middleware/auth')
 
 router.get('/landingpage', landingPage);
@@ -8,5 +8,6 @@ router.get('/:id/detail', detailPage);
 router.get('/category', category);
 router.post('/checkout', isLoginPlayer, checkout);
 router.get('/history', isLoginPlayer, history);
+router.get('/history/:id/detail', isLoginPlayer, historyDetail);
 
 module.exports = router;
